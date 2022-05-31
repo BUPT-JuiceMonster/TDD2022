75ee1b76910e6b6dfb78aa82d86d4b1e17f10347
Haocheng Liu <lhc_928@bupt.edu.cn>
2022-06-01 05:03:15 +0800

    good

--- superlists/settings.py
+++ superlists/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'lists',
 ]
 
+CSRF_TRUSTED_ORIGINS = ["http://139.224.66.172"]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
